a6e8e2efdaa0c8a246e64f383bd60b7781d6a413
viniciusmarinelli <vinicius.marinelli@outlook.com>
2026-03-28 01:27:22 -0300

    feat: Criar arquivo e função para exportar para o Excel o resultado

---
+++ src/report.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import os
+# import win32com.client as win32
+import time 
+from datetime import datetime
+
+def generate_report(df_third_leg, df_lean_cattle_saturday):
+    with pd.ExcelWriter('report.xlsx') as writer:
+        df_third_leg.to_excel(writer, sheet_name='Third Leg', index=False)
+        df_lean_cattle_saturday.to_excel(writer, sheet_name='Lean Cattle Saturday', index=False)
+    return "Relatório gerado com sucesso!"
